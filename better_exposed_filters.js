44811cc728ca105e9f7104839bdb11a1d09fa562
Mike Keran <info@mikekeran.com>
2011-05-09 10:12:44 -0700

    Issue #1111712 by mikeker: Adds Links as an option for filters

--- better_exposed_filters.js
+++ better_exposed_filters.js
@@ -1,3 +1,8 @@
+/**
+ * @file better_exposed_filters.js
+ * 
+ * Provides some client-side functionality for the Better Exposed Filters module
+ */
 (function ($) {
   Drupal.behaviors.better_exposed_filters = { 
     attach: function() {
